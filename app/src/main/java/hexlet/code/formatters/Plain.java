b768a26a822e4e071b0211e6bcd137bab6305550
ymirotvorenie <karoline.navarra@gmail.com>
2024-04-30 16:00:40 +0700

    update tests

--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -7,19 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Plain {
-//    Property 'chars2' was updated. From [complex value] to false
-//    Property 'checked' was updated. From false to true
-//    Property 'default' was updated. From null to [complex value]
-//    Property 'id' was updated. From 45 to null
-//    Property 'key1' was removed
-//    Property 'key2' was added with value: 'value2'
-//    Property 'numbers2' was updated. From [complex value] to [complex value]
-//    Property 'numbers3' was removed
-//    Property 'numbers4' was added with value: [complex value]
-//    Property 'obj1' was added with value: [complex value]
-//    Property 'setting1' was updated. From 'Some value' to 'Another value'
-//    Property 'setting2' was updated. From 200 to 300
-//    Property 'setting3' was updated. From true to 'none'
+
     public static String output(List<DiffElement> difference) {
         StringBuilder result = new StringBuilder();
 
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -3,6 +3,8 @@ package hexlet.code;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static hexlet.code.Differ.generate;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
@@ -14,32 +16,7 @@ public class DifferTest {
 
     @Test
     public void testJsonDiffs() throws IOException {
-        String expected = """
-                {
-                    chars1: [a, b, c]
-                  - chars2: [d, e, f]
-                  + chars2: false
-                  - checked: false
-                  + checked: true
-                  - default: null
-                  + default: [value1, value2]
-                  - id: 45
-                  + id: null
-                  - key1: value1
-                  + key2: value2
-                    numbers1: [1, 2, 3, 4]
-                  - numbers2: [2, 3, 4, 5]
-                  + numbers2: [22, 33, 44, 55]
-                  - numbers3: [3, 4, 5]
-                  + numbers4: [4, 5, 6]
-                  + obj1: {nestedKey=value, isNested=true}
-                  - setting1: Some value
-                  + setting1: Another value
-                  - setting2: 200
-                  + setting2: 300
-                  - setting3: true
-                  + setting3: none
-                }""";
+        String expected = Files.readString(Paths.get("./src/test/resources/result_stylish.txt"));
 
         assertEquals(generate("./src/test/resources/file1.json",
                 "./src/test/resources/file2.json", "stylish"), expected);
@@ -47,36 +24,34 @@ public class DifferTest {
 
     @Test
     public void testYamlDiffs() throws IOException {
-        String expected = """
-                {
-                    chars1: [a, b, c]
-                  - chars2: [d, e, f]
-                  + chars2: false
-                  - checked: false
-                  + checked: true
-                  - default: null
-                  + default: [value1, value2]
-                  - id: 45
-                  + id: null
-                  - key1: value1
-                  + key2: value2
-                    numbers1: [1, 2, 3, 4]
-                  - numbers2: [2, 3, 4, 5]
-                  + numbers2: [22, 33, 44, 55]
-                  - numbers3: [3, 4, 5]
-                  + numbers4: [4, 5, 6]
-                  + obj1: {nestedKey=value, isNested=true}
-                  - setting1: Some value
-                  + setting1: Another value
-                  - setting2: 200
-                  + setting2: 300
-                  - setting3: true
-                  + setting3: none
-                }""";
+        String expected = Files.readString(Paths.get("./src/test/resources/result_stylish.txt"));
 
         assertEquals(generate("./src/test/resources/file1.yml",
                 "./src/test/resources/file2.yml", "stylish"), expected);
     }
+
+    @Test
+    public void testEqualFiles() throws IOException {
+        String expected = Files.readString(Paths.get("./src/test/resources/result_equal_files.txt"));
+        assertEquals(generate("./src/test/resources/file2.json",
+                "./src/test/resources/file2.json", "stylish"), expected);
+    }
+
+    @Test
+    public void testPlainFormatter() throws IOException {
+        String expected = Files.readString(Paths.get("./src/test/resources/result_plain.txt"));
+
+        assertEquals(generate("./src/test/resources/file1.yml",
+                "./src/test/resources/file2.yml", "plain"), expected);
+    }
+
+//    @Test
+//    public void testJsonFormatter() throws IOException {
+//        String expected = Files.readString(Paths.get("./src/test/resources/result_json.txt"));
+//
+//        assertEquals(generate("./src/test/resources/file1.yml",
+//                "./src/test/resources/file2.yml", "json"), expected);
+//    }
 
     @Test
     public void testWrongFileExtension() {
@@ -89,50 +64,11 @@ public class DifferTest {
     }
 
     @Test
-    public void testEqualFiles() throws IOException {
-        String expected = """
-                {
-                    chars1: [a, b, c]
-                    chars2: false
-                    checked: true
-                    default: [value1, value2]
-                    id: null
-                    key2: value2
-                    numbers1: [1, 2, 3, 4]
-                    numbers2: [22, 33, 44, 55]
-                    numbers4: [4, 5, 6]
-                    obj1: {nestedKey=value, isNested=true}
-                    setting1: Another value
-                    setting2: 300
-                    setting3: none
-                }""";
-        assertEquals(generate("./src/test/resources/file2.json",
-                "./src/test/resources/file2.json", "stylish"), expected);
-    }
-
-    @Test
-    public void testPlainFormatter() throws IOException {
-        String expected = """
-                Property 'chars2' was updated. From [complex value] to false
-                Property 'checked' was updated. From false to true
-                Property 'default' was updated. From null to [complex value]
-                Property 'id' was updated. From 45 to null
-                Property 'key1' was removed
-                Property 'key2' was added with value: 'value2'
-                Property 'numbers2' was updated. From [complex value] to [complex value]
-                Property 'numbers3' was removed
-                Property 'numbers4' was added with value: [complex value]
-                Property 'obj1' was added with value: [complex value]
-                Property 'setting1' was updated. From 'Some value' to 'Another value'
-                Property 'setting2' was updated. From 200 to 300
-                Property 'setting3' was updated. From true to 'none'
-                """;
-        assertEquals(generate("./src/test/resources/file1.yml",
-                "./src/test/resources/file2.yml", "plain"), expected);
-    }
-
-    @Test
-    public void testJsonFormatter() {
-
+    public void testWrongFormat() {
+        var thrown = catchThrowable(
+                () -> generate("./src/test/resources/file1.yml",
+                        "./src/test/resources/file2.yml", "wrong")
+        );
+        assertThat(thrown).isInstanceOf(IllegalStateException.class);
     }
 }
